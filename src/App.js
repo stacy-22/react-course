4ec6356aefa5c5c6c5ea4868c010c61c4e15e4d6
Stacy Faizulina <stacy.faizulina@shopify.com>
2022-05-29 13:56:31 +0100

    added js tools

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h2", {}, name),
-        React.createElement("h3", {}, animal),
-        React.createElement("h3", {}, breed)
-    ])
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {id: "my-brand"}, "Adopt Me!"),
-            React.createElement(Pet, {name: "Luna", animal: "Dog", breed: "Havanese"}),
-            React.createElement(Pet, {name: "Pepper", animal: "Bird", breed: "Cockatiel"}),
-            React.createElement(Pet, {name: "Sudo", animal: "Dog", breed: "Terrier"})
-        ]
-    );
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Sudo", animal: "Dog", breed: "Terrier" }),
+  ]);
+};
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
