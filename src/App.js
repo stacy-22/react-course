d27013a14c5196151866a8bc1a258eaa3f549ef6
Stacy Faizulina <stacy.faizulina@shopify.com>
2022-06-04 22:14:07 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1 id="my-brand">Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1 id="my-brand">Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>Details mock page</h2>;
+};
+
+export default Details;
